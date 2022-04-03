3d29b24aa1caf757042e05400988464b06b45b15
Alina <alina.marschel@gmail.com>
2022-04-03 18:42:53 +0200

    Sphere and Box

--- marble_run/src/Experience/World/World.js
+++ marble_run/src/Experience/World/World.js
@@ -10,11 +10,19 @@ export default class World
         this.scene = this.experience.scene
 
         const sphere = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.SphereGeometry( 1, 32, 16 ),
             new THREE.MeshBasicMaterial({color: 0xffff00})
         )
 
-        this.scene.add(sphere)
+        const box = new THREE.Mesh(
+            new THREE.BoxGeometry(10,1,1),
+            new THREE.MeshBasicMaterial({color: 0xff25ab})
+        )
+
+        box.position.z = 2
+        box.rotation.y = -.15
+
+        this.scene.add(sphere,box)
 
     }
 }
