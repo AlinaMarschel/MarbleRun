30dd7ed4aa7fe27f193e7620b64a1c111375ab8f
Alina <alina.marschel@gmail.com>
2022-04-03 22:14:56 +0200

    Marble Object

---
+++ marble_run/src/Experience/World/Marble.js
@@ -0,0 +1,21 @@
+import * as THREE from 'three'
+
+export default class Marble 
+{
+    constructor()
+    {
+
+        let color = this.color
+        let size = this.size
+        let weight = this.weight
+        let velocity = this.velocity
+
+
+        
+    }
+
+    createSphere(color, size, weight, velocity)
+    {
+        
+    }
+}
