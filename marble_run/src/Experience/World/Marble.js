1de6b7dca41e8982033e29798775de909a7d1d16
Alina <alina.marschel@gmail.com>
2022-04-04 13:54:18 +0200

    Orbit Controls ausgestellt

--- marble_run/src/Experience/World/Marble.js
+++ marble_run/src/Experience/World/Marble.js
@@ -1,21 +1,27 @@
 import * as THREE from 'three'
+import Experience from "../Experience";
 
 export default class Marble 
 {
     constructor()
     {
+        this.experience = new Experience()
+        this.scene = this.experience.scene
 
         let color = this.color
         let size = this.size
         let weight = this.weight
         let velocity = this.velocity
 
-
-        
     }
 
     createSphere(color, size, weight, velocity)
     {
         
+    }
+
+    addSphere()
+    {
+        this.scene.add()
     }
 }
