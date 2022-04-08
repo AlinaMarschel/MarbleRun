7ea0cb67bb9b081019982c2113b0f0e0b273368f
Pia <pia.starzinski@stud.hshl.de>
2022-04-08 14:53:24 +0200

    Slider

--- marble_run/src/script.js
+++ marble_run/src/script.js
@@ -2,3 +2,17 @@ import './style.css'
 import Experience from './Experience/Experience.js'
 
 const experience = new Experience(document.querySelector('canvas.webgl'))
+
+        var slider = document.getElementById("slider");
+        var selector = document.getElementById("selector");
+        var SelectValue = document.getElementById("SelectValue");
+
+        SelectValue.innerHTML = slider.value;
+
+
+        slider.oninput = function(){
+            selector.style.left = this.value + "%";
+        SelectValue.innerHTML = this.value;
+
+        }
+    
